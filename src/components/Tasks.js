e896d39eb75467d24fe0dd40205e22684e7ab852
rhallgarth88 <59847530+rhallgarth88@users.noreply.github.com>
2021-02-25 01:35:35 -0500

    Implement form for adding tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,7 +3,7 @@ import Task from './Task'
 
 
 
-const Tasks = ({ tasks, onDelete }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
 
 
 
@@ -11,7 +11,7 @@ const Tasks = ({ tasks, onDelete }) => {
         <>
             {tasks.map((task) => (
 
-            <Task key={task.id} task={task} onDelete={onDelete} />
+            <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
 
             ))}
         </>
